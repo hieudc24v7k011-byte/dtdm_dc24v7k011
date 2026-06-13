1d4d2fe3e3420318462132e04df87ebb5dcf4537
hieudc24v7k011-byte <hieudc24v7k011@vlvh.ctu.edu.vn>
2026-06-13 14:53:01 +0700

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6fcab23ac15ad39cc26ca84cee6b0f29d00239ab',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6fcab23ac15ad39cc26ca84cee6b0f29d00239ab',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'platformsh/config-reader' => array(
+            'pretty_version' => '3.0.1',
+            'version' => '3.0.1.0',
+            'reference' => 'cab2b6c0cdfc318df09a64136957dc1fc308f418',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../platformsh/config-reader',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
